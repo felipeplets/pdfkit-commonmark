727ea1206dd95698017aea7f70a90aabf75723f7
Felipe Plets <felipe.plets@menvia.com>
2018-11-15 13:50:36 -0200

    Add dist to be able to use it via git repo

---
+++ dist/default-options.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports['default'] = {
+    fontSize: 12,
+    fonts: {
+        'default': 'Times-Roman',
+        bold: 'Times-Bold',
+        italic: 'Times-Italic',
+        'bold-italic': 'Times-BoldItalic',
+        'heading-bold': 'Helvetica-Bold',
+        'heading-default': 'Helvetica',
+        'code': 'Courier'
+    }
+};
